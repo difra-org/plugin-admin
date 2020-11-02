e05cad633ea53c370ce523032277faf05a235816
Eduard Kutuev <ed@napych.com>
2020-11-02 21:43:41 +0300

    Added missing files

---
+++ main.php
@@ -0,0 +1,3 @@
+<?php
+
+\Difra\Adm\Plugin::enable();
---
+++ src/Plugin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Difra\Adm;
+
+/**
+ * Class Plugin
+ * @package Difra\Adm
+ */
+class Plugin extends \Difra\Plugin
+{
+    /**
+     * @inheritdoc
+     */
+    protected function init()
+    {
+    }
+}
+
