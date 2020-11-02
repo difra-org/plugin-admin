9628beea2efb978f1c5c5f75da5ac5a483b3613d
Eduard Kutuev <ed@napych.com>
2020-11-02 20:51:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Difra Administration interface
+
+Administration interface for Difra Framework
+
+## Links
+- [Website](https://www.difra.org/)
+- [Source code and releases](https://github.com/difra-org)
+- [Issues](https://github.com/difra-org/difra/issues)
---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'instances' => [
+        'adm' => [
+            'include' => [
+                'jquery3' => true,
+                'bootstrap' => true,
+                'all' => true,
+                'fontawesome5' => true
+            ]
+        ],
+    ]
+];
